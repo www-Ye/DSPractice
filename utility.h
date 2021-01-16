1dea2db8a4984d6258282f0186ebae62658be0ee
wy <919872507@qq.com>
2021-01-16 11:11:24 +0800

    add utility.cc,utility.h

---
+++ utility.h
@@ -0,0 +1,19 @@
+#ifndef _UTILITY_H_
+#define _UTILITY_H_
+
+using namespace std;
+#include <iostream>
+#include <limits>
+#include <cmath>
+#include <cstdlib>
+#include <cstddef>
+#include <fstream>
+#include <cctype>
+#include <ctime>
+bool user_says_yes();
+
+enum Error_code{ success, fail, range_error, underflow, 
+    overflow, fatal, not_present, duplicate_error, 
+    entry_inserted, entry_found, internal_error };
+
+#endif
